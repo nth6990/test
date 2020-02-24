e7a426b9d33532205b560fee6694fc74bba3c276
nth6990 <56827359+nth6990@users.noreply.github.com>
2020-02-24 21:42:22 +0900

    error

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.test;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.FrameLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Fragment1 fragment1 = new Fragment1();
+
+        getSupportFragmentManager().beginTransaction().add(R.id.container,fragment1).commit();
     }
 }
